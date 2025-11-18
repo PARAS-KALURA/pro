c6acfecbf81828be3389902b6082f55f64269cfa
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-11-18 16:14:43 +0530

    logic done

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const item = document.getElementById('item');
 const amount = document.getElementById('amount');
+const above = document.getElementById('above');
 
-const card = document.getElements(".card");
+const cards = document.querySelectorAll(".card");
 
+
+
+let totalItem = 0;
+let totalAmount = 0;
+
+// Loop through all cards
+
+cards.forEach((card) => {
+    card.addEventListener("click", () => {
+        const priceText = card.querySelector('.price').innerText;
+        const price = parseInt(priceText.replace('Rs.', '').trim());
+              
+
+
+        totalItem++;
+        totalAmount += price;
+
+        item.innerHTML = `TOTAL ITEMS: ${totalItem}`;
+        amount.innerHTML = `TOTAL AMOUNT: Rs.${totalAmount}`;
+
+        above.innerText = totalItem;
+
+    })
+})
